f46f2b79b1f87b2be1e9e2f3f01bf0fd005c9a0a
Nechirvan Zibar <nechirvan.zibar@bwedu.de>
2024-06-15 13:16:56 +0200

    Basic view folder

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/:filename', (req, res) => {
+app.get('/view/:filename', (req, res) => {
     const filename = req.params.filename;
-    res.sendFile(path.join(__dirname, filename));
+    res.status(200).sendFile(path.join(__dirname + '/view', filename));
 });
 
 app.listen(port, () => {
