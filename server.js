46b899f7d4200d53c2ea4008c2b67c66e6ee9cb3
lobosaka <maxphiliplabuszka@gmail.com>
2024-06-17 18:05:28 +0200

    Anpassung an Präsi

--- server.js
+++ server.js
@@ -4,7 +4,45 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"))
+//CRUD - Create, Read, Update, Delete
+//Grundlegende Operationen von Servern
+
+// Read
+app.get("/", (req, res) => {
+    res.send("<h1>Hallo Welt</h1>")
+})
+
+app.get("/user", (req, res) => {
+    res.send("<h1>Hallo User</h1>")
+})
+
+app.get("/user/max", (req, res) => {
+    res.send("<h1>Hallo Max</h1>")
+})
+
+app.get("/user/danny", (req, res) => {
+    res.send("<h1>Hallo Danny</h1>")
+})
+
+app.get("/user/nechirvan", (req, res) => {
+    res.send("<h1>Hallo Nechirvan</h1>")
+})
+
+// Create
+app.post("/", (req, res) => {
+    res.send("<h1>POST: Hallo Welt</h1>")
+})
+
+// Update
+app.put("/", (req, res) => {
+    res.send("<h1>PUT: Hallo Welt</h1>")
+})
+
+// Delete
+app.delete("/", (req, res) => {
+    res.send("<h1>DELETE: Hallo Welt</h1>")
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
