b5564dc675afc2c7a07624a08af5f996d7957daa
Nechirvan Zibar <nechirvan.zibar@bwedu.de>
2024-06-17 23:11:34 +0200

    Restructuring

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require('express');
-const path = require('path');
 const userRouter = require('./routes/users')
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs')
 app.use(express.static("public"))
-app.use(express.urlencoded( {extended: true}))
+app.use(express.urlencoded({extended: true}))
 
 app.use('/routes/users', userRouter)
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-
+app.set('view engine', 'ejs')
 app.get('/forms', (req, res) => {
     res.render('forms');
 });
