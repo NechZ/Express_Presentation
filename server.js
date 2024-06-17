e98c5b6a7ff6e7b870099520d8d3d9161ab96c5a
lobosaka <maxphiliplabuszka@gmail.com>
2024-06-17 23:20:48 +0200

    safe

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-//CRUD - Create, Read, Update, Delete
-//Grundlegende Operationen von Servern
-
 // Read
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
@@ -53,6 +50,5 @@ app.listen(port, () => {
 
 
 
-
-
 //app.use(express.static('public'));
+
--- server05.js
+++ server05.js
@@ -3,9 +3,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-//CRUD - Create, Read, Update, Delete
-//Grundlegende Operationen von Servern
-
 //Read
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
